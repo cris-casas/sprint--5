08c40d48cc5aec29e8ab62d372f51a88fad80890
Cris-casas-react <cristina.casas2@gmail.com>
2023-03-03 22:06:44 +0100

    ex-2

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+const contenido = document.getElementById("contenido_acudit");
+
+function mostrarAcudit() {
+
+    fetch('https://icanhazdadjoke.com/', { 
+        headers: {
+            'Accept': 'application/json'
+        }})
+        .then(response => response.json())
+        .then(json => contenido.innerHTML=json.joke);
+
+}
+
+/*
+
 const API_URL = "https://icanhazdadjoke.com/";
 
 function mostrarAcudit() {
@@ -10,3 +25,5 @@ function mostrarAcudit() {
         .then(json => console.log(json));
 
 }
+
+*/
